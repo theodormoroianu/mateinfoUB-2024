ac9e8d18ac651d08f07d5d6cfe274c98a2c14450
tifui-alexandru <itifui00@gmail.com>
2024-05-17 14:26:15 +0200

    done tests for prajitura-cu-mujdei

---
+++ prajitura-cu-mujdei/sol_impossibil.cpp
@@ -0,0 +1,13 @@
+/**
+ * Author: Theodor Moroianu
+ * Intended: 100
+ * Complexity: O(A + B)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout << "IMPOSIBIL";
+  return 0;
+}
